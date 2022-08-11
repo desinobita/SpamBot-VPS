1a1af62b5e59d6c6599d2a5564a018b40a3ec08e
RiZoeL -! <85222112+MrRizoel@users.noreply.github.com>
2022-08-11 15:39:03 +0530

    Update __init__.py

--- RiZoeLXSpam/__init__.py
+++ RiZoeLXSpam/__init__.py
@@ -20,25 +20,47 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
 
 rizoelversion = "v2.0.3"
 
-#values
+def clr():
+    if os.name == 'nt':
+        os.system('cls')
+    else:
+        os.system('clear')
 
-API_ID = ""  #Fill Api ID 
-API_HASH = ""  #Fill Api Hash
-ALIVE_PIC = ""  #Alive pic (telegraph link)
-CMD_HNDLR = ""  #Cmd Handler
-BOT_TOKEN = ""   # Bot Token 
-BOT_TOKEN2 = ""  # Bot Token
-BOT_TOKEN3 = ""  # Bot Token
-BOT_TOKEN4 = ""  # Bot Token
-BOT_TOKEN5 = ""  # Bot Token
-BOT_TOKEN6 = ""  # Bot Token
-BOT_TOKEN7 = ""  # Bot Token
-BOT_TOKEN8 = ""  # Bot Token
-BOT_TOKEN9 = ""  # Bot Token
-BOT_TOKEN10 = "" # Bot Token
-OWNER_ID = "" # Owner Id (Only One Owner id don't Fill 2-3 ids)
-SUDO_USERS = []  #fill sudo uses's id in block i.e. [122366538, 3935715262]
+print("\n Starting RiZoeL X Spam ....")
+clr()
+print(" • RiZoeL X Spam • ")
+print("\n\n Enter Valid Details and Continue")
 
+API_ID = int(input("\n Enter api id: "))
+API_HASH = str(input("\n Enter Api Hash: "))
+ALIVE_PIC = str(input("\n Enter Telegraph Link For alive pic else press enter: ")) 
+CMD_HNDLR = input("\n Cmd Handler: ")
+
+print("\n Note: You Have to fill all bot tokens -!\n")
+BOT_TOKEN = str(input("Enter Bot Token 1: "))
+BOT_TOKEN2 = str(input("Enter Bot Token 2: "))
+BOT_TOKEN3 = str(input("Enter Bot Token 3: "))
+BOT_TOKEN4 = str(input("Enter Bot Token 4: "))
+BOT_TOKEN5 = str(input("Enter Bot Token 5: "))
+BOT_TOKEN6 = str(input("Enter Bot Token 6: "))
+BOT_TOKEN7 = str(input("Enter Bot Token 7: "))
+BOT_TOKEN8 = str(input("Enter Bot Token 8: "))
+BOT_TOKEN9 = str(input("Enter Bot Token 9: "))
+BOT_TOKEN10 = str(input("Enter Bot Token 10: "))
+OWNER_ID = int(input("single Owner id: "))
+sudo = str(input("Sudo Users id Space by Space or Press enter: "))
+
+
+def make_int(str_input):
+    str_list = str_input.split(" ")
+    int_list = []
+    for x in str_list:
+        int_list.append(int(x))
+    return int_list
+
+SUDO_USERS = []
+if sudo:
+    SUDO_USERS = make_int(sudo)
 
 #Tokens
 
