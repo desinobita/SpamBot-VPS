dc9a6d6d8b05ce8c36a9c307aaa76636bf23fa1b
RiZoeL -! <85222112+MrRizoel@users.noreply.github.com>
2022-08-13 01:45:16 +0530

    Create Values.py

---
+++ Values.py
@@ -0,0 +1,38 @@
+import os
+
+
+
+def clr():
+    if os.name == 'nt':
+        os.system('cls')
+    else:
+        os.system('clear')
+
+
+print("\n Starting RiZoeL X Spam ....")
+clr()
+print(" • RiZoeL X Spam • ")
+print("\n\n Enter Valid Details and Continue")
+
+API_ID = int(input("\n Enter api id: "))
+API_HASH = str(input("\n Enter Api Hash: "))
+alivee = input("\n Enter Telegraph Link For alive pic else press enter: ")
+hndlr = input("\n Cmd Handler: ")
+
+print("\n Note: You Have to fill all bot tokens -!\n")
+BOT_TOKEN = str(input("Enter Bot Token 1: "))
+BOT_TOKEN2 = str(input("Enter Bot Token 2: "))
+BOT_TOKEN3 = str(input("Enter Bot Token 3: "))
+BOT_TOKEN4 = str(input("Enter Bot Token 4: "))
+BOT_TOKEN5 = str(input("Enter Bot Token 5: "))
+BOT_TOKEN6 = str(input("Enter Bot Token 6: "))
+BOT_TOKEN7 = str(input("Enter Bot Token 7: "))
+BOT_TOKEN8 = str(input("Enter Bot Token 8: "))
+BOT_TOKEN9 = str(input("Enter Bot Token 9: "))
+BOT_TOKEN10 = str(input("Enter Bot Token 10: "))
+owner = int(input("single Owner id: "))
+sudo = input("Sudo Users id Space by Space or Press enter: ")
+
+clr()
+
+print("<= Staring Your RiZoeL X Spam =>")
