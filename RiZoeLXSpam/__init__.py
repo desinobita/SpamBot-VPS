f30226b47d9d148a9672b81096ccfd7d4dc1cf4c
RiZoeL -! <85222112+MrRizoel@users.noreply.github.com>
2022-08-13 01:05:23 +0530

    Update __init__.py

--- RiZoeLXSpam/__init__.py
+++ RiZoeLXSpam/__init__.py
@@ -9,7 +9,7 @@ import telethon.utils
 from telethon import TelegramClient, events
 import logging
 import time
-
+from main import API_ID, API_HASH, alivee, BOT_TOKEN, BOT_TOKEN2, BOT_TOKEN3, BOT_TOKEN4, BOT_TOKEN5, BOT_TOKEN6, BOT_TOKEN7, BOT_TOKEN8, BOT_TOKEN9, BOT_TOKEN10, sudo, owner, hndlr
 
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.WARNING)
@@ -18,39 +18,9 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
 
 rizoelversion = "v2.0.3"
 
-def clr():
-    if os.name == 'nt':
-        os.system('cls')
-    else:
-        os.system('clear')
-
-print("\n Starting RiZoeL X Spam ....")
-clr()
-print(" • RiZoeL X Spam • ")
-print("\n\n Enter Valid Details and Continue")
-
-API_ID = int(input("\n Enter api id: "))
-API_HASH = str(input("\n Enter Api Hash: "))
-ALIVE_PIC = input("\n Enter Telegraph Link For alive pic else press enter: ")
-CMD_HNDLR = input("\n Cmd Handler: ")
-
-print("\n Note: You Have to fill all bot tokens -!\n")
-BOT_TOKEN = str(input("Enter Bot Token 1: "))
-BOT_TOKEN2 = str(input("Enter Bot Token 2: "))
-BOT_TOKEN3 = str(input("Enter Bot Token 3: "))
-BOT_TOKEN4 = str(input("Enter Bot Token 4: "))
-BOT_TOKEN5 = str(input("Enter Bot Token 5: "))
-BOT_TOKEN6 = str(input("Enter Bot Token 6: "))
-BOT_TOKEN7 = str(input("Enter Bot Token 7: "))
-BOT_TOKEN8 = str(input("Enter Bot Token 8: "))
-BOT_TOKEN9 = str(input("Enter Bot Token 9: "))
-BOT_TOKEN10 = str(input("Enter Bot Token 10: "))
-OWNER_ID = int(input("single Owner id: "))
-sudo = input("Sudo Users id Space by Space or Press enter: ")
-
-clr()
-
-print("Staring Your RiZoeL X Spam")
+ALIVE_PIC = alivee
+OWNER_ID = int(owner)
+CMD_HNDLR = hndlr
 
 def make_int(str_input):
     str_list = str_input.split(" ")
